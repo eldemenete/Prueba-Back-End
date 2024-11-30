bf7853329616b68b94ed71b64d0e10073197929e
Gera <gera.esteban.caqueo@gmail.com>
2024-11-30 02:35:39 -0300

    aaaa
    aaaaa

--- PRUEBA/inventarioVeterinariaGyI/gestorProductos/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def productos_list(request):
-    return render(request, 'productos.html'),
-
-def categorias_list(request):
-    return render(request, 'categorias.html'),
--- PRUEBA/inventarioVeterinariaGyI/gestorUser/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from gestorUser.views import SignUpView
-
-urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-]
